e2be191196c3a282dac3d59f02c22e9e4871513e
mitchamador <vicokenator@gmail.com>
2017-05-16 23:47:00 +0300

    Initial commit

---
+++ src/by/mitchamador/Main.java
@@ -0,0 +1,304 @@
+package by.mitchamador;
+
+import com.iptv.parser.M3UFile;
+import com.iptv.parser.M3UHead;
+import com.iptv.parser.M3UItem;
+import com.iptv.parser.M3UToolSet;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import by.mitchamador.XMLTV.Channel;
+import by.mitchamador.XMLTV.Programme;
+import by.mitchamador.XMLTV.DisplayName;
+
+public class Main {
+
+    static String[] filelistM3U;
+    static String[] filelistXmlTv;
+
+    static String out;
+    static boolean quiet;
+    static boolean debug;
+    static int threads = 1;
+
+    static void parseArgs(String[] args) {
+        ArrayList<String> m3uList = new ArrayList<String>();
+        ArrayList<String> xmlTvList = new ArrayList<String>();
+        if (args != null) {
+            int c = 0;
+            while (c < args.length) {
+                String arg = args[c].toLowerCase();
+                if ("-u".equals(arg)) {
+                    c++;
+                    if (c < args.length) {
+                        m3uList.add(args[c]);
+                    }
+                } else if ("-x".equals(arg)) {
+                    c++;
+                    if (c < args.length) {
+                        xmlTvList.add(args[c]);
+                    }
+                } else if ("-j".equals(arg)) {
+                    c++;
+                    if (c < args.length) {
+                        try {
+                            threads = Integer.parseInt(args[c]);
+                        } catch (NumberFormatException e) {
+                        }
+                    }
+                } else if ("-o".equals(arg)) {
+                    c++;
+                    if (c < args.length) {
+                        out = args[c];
+                    }
+                } else if ("-q".equals(arg)) {
+                    quiet = true;
+                } else if ("-d".equals(arg)) {
+                    debug = true;
+                }
+                c++;
+            }
+        }
+        filelistM3U = m3uList.toArray(new String[m3uList.size()]);
+        filelistXmlTv = xmlTvList.toArray(new String[xmlTvList.size()]);
+    }
+
+
+    static String getConvertedName(String s) {
+        return s.toLowerCase().replaceAll("[ _\\-\\(\\)]", "");
+    }
+
+    public static void main(String[] args) {
+        long t1, t2;
+
+        parseArgs(args);
+
+        XMLTV epg = new XMLTV();
+
+        ExecutorService pool = Executors.newFixedThreadPool(threads <= 0 ? Runtime.getRuntime().availableProcessors() : threads);
+
+        t1 = new Date().getTime();
+
+        ArrayList<Future<XMLTV>> futures = new ArrayList<Future<XMLTV>>();
+
+        for (final String file : filelistXmlTv) {
+            futures.add(pool.submit(new Callable<XMLTV>() {
+                @Override
+                public XMLTV call() throws Exception {
+                    XMLTV epg = new XMLTV();
+
+                    try {
+                        Long t1 = new Date().getTime();
+                        epg.parseStax(file);
+                        Long t2 = new Date().getTime();
+
+                        if (debug) {
+                            System.out.println(file + " " + (double) (t2 - t1) / (double) 1000 + " sec, total channels: " + epg.channels.size() + ", total programmes: " + epg.programmes.size());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return epg;
+                }
+            }));
+        }
+        pool.shutdown();
+
+        for (Future<XMLTV> future : futures) {
+            try {
+                XMLTV x = future.get();
+                epg.channels.addAll(x.channels);
+                epg.programmes.addAll(x.programmes);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                System.out.println(e.getMessage());
+                System.exit(1);
+                //e.printStackTrace();
+            }
+        }
+
+        t2 = new Date().getTime();
+
+        if (debug) {
+            System.out.println("total time for all files: " + (double) (t2 - t1) / (double) 1000);
+        }
+
+        Collections.sort(epg.channels, new Comparator<Channel>() {
+            @Override
+            public int compare(Channel o1, Channel o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        int countM3U = 0;
+
+        HashSet<String> aliases = new HashSet<String>();
+        HashSet<String> missed = new HashSet<String>();
+        HashMap<String, Channel> channelsXML = new HashMap<String, Channel>();
+
+        for (String file : filelistM3U) {
+            M3UFile m3u = M3UToolSet.load(file);
+
+            M3UHead head = m3u.getHeader();
+
+            for (M3UItem item : m3u.getItems()) {
+                String nameM3U;
+                if (item.getTvgName() != null) {
+                    nameM3U = item.getTvgName().replaceAll("_", " ");
+                } else {
+                    nameM3U = item.getChannelName();
+                }
+
+                boolean find = false;
+                for (Channel ch : epg.channels) {
+                    for (String nameXML : ch.getNameList()) {
+                        if (nameXML.equals(nameM3U)) {
+                            find = true;
+                        } else if (getConvertedName(nameXML).equals(getConvertedName(nameM3U))) {
+                            find = true;
+                            if (!aliases.contains(nameM3U + ":" + nameXML)) {
+                                aliases.add(nameM3U + ":" + nameXML);
+                            }
+                        }
+                        if (find) {
+                            channelsXML.put(ch.id, ch);
+                            break;
+                        }
+                    }
+                    if (find) {
+                        break;
+                    }
+                }
+
+                if (!find) {
+                    missed.add(nameM3U);
+                }
+
+                countM3U++;
+            }
+        }
+
+
+        if (!quiet && !aliases.isEmpty()) {
+            System.out.println("=== aliases ===");
+            for (String s : aliases) {
+                System.out.println(s);
+            }
+        }
+
+        if (!quiet && !missed.isEmpty()) {
+            System.out.println("=== missed channels ===");
+            for (String s : missed) {
+                System.out.println(s);
+            }
+        }
+
+        if (!quiet) {
+            System.out.println("total channels: m3u - " + countM3U + ", aliases - " + aliases.size() +
+                    ", missed - " + missed.size() + ", xmltv - " + channelsXML.size());
+        }
+
+        if (out != null) {
+            StringBuilder xml = new StringBuilder(512 * 1024);
+            xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            xml.append("<tv>\n");
+
+            for (Channel ch : channelsXML.values()) {
+                xml.append(ch.toXml(1));
+            }
+
+/*
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.HOUR_OF_DAY, (c.get(Calendar.HOUR_OF_DAY) / 6) * 6);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            long begin = c.getTime().getTime();
+            c.add(Calendar.HOUR_OF_DAY, 3 * 24);
+            long end = c.getTime().getTime();
+
+            for (Programme p : epg.programmes) {
+                Channel ch = channelsXML.get(p.channel);
+                if (ch != null && p.start != null && p.stop != null
+                        && ((p.start.getTime() >= begin && p.start.getTime() <= end) || (p.stop.getTime() >= begin && p.stop.getTime() <= end))
+                        ) {
+                    xml.append(p.toXml(1));
+                }
+            }
+*/
+            for (Programme p : epg.programmes) {
+                Channel ch = channelsXML.get(p.channel);
+                if (ch != null && p.start != null && p.stop != null) {
+                    xml.append(p.toXml(1));
+                }
+            }
+
+            xml.append("</tv>\n");
+
+            try {
+                saveFile(out, xml.toString().getBytes("UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (debug) {
+            System.out.println("done.");
+        }
+
+    }
+
+    private static void saveFile(String filePath, byte[] buffer) throws IOException {
+        FileOutputStream fo = null;
+        try {
+            fo = new FileOutputStream(filePath);
+            GZIPOutputStream gzip = filePath.endsWith(".gz") ? new GZIPOutputStream(fo) : null;
+            OutputStream out = gzip == null ? fo : gzip;
+
+            ByteArrayInputStream bi = new ByteArrayInputStream(buffer);
+            byte[] buf = new byte[262144];
+
+            int x;
+            while ((x = bi.read(buf)) != -1) {
+                out.write(buf, 0, x);
+            }
+
+            if (gzip != null) {
+                gzip.finish();
+            }
+
+            out.flush();
+        } catch (IOException e) {
+        } finally {
+            if (fo != null) fo.close();
+        }
+    }
+
+    private static byte[] readFile(String filename) throws IOException {
+        BufferedInputStream fin = new BufferedInputStream(new FileInputStream(filename));
+        GZIPInputStream gzip = null;
+        if (filename.endsWith(".gz")) {
+            gzip = new GZIPInputStream(fin);
+        }
+
+        InputStream in = gzip != null ? gzip : fin;
+
+        byte[] buffer = new byte[262144];
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        }
+
+        in.close();
+        out.close();
+
+        return out.toByteArray();
+    }
+
+}
---
+++ src/by/mitchamador/XMLTV.java
@@ -0,0 +1,448 @@
+package by.mitchamador;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+
+public class XMLTV {
+
+    //@SerializedName("channel")
+    public List<Channel> channels = new ArrayList<Channel>();
+    //@SerializedName("programme")
+    public List<Programme> programmes = new ArrayList<Programme>();
+
+    public class Programme {
+        //@SerializedName("@channel")
+        public String channel;
+        //@SerializedName("@start")
+        public Date start;
+        //@SerializedName("@stop")
+        public Date stop;
+
+        //@SerializedName("title")
+        public Title title;
+
+        //@SerializedName("sub-title")
+        public Title subtitle = null;
+
+        //@SerializedName("desc")
+        public Title description = null;
+
+//  //@SerializedName("previously-shown") 
+//  public PreviousShow previousShow = new PreviousShow(); 
+
+        //@SerializedName("episode-num")
+        public EpisodeNumber episodeNumber;
+
+        //@SerializedName("category")
+        public List<Category> categories = new ArrayList<Category>();
+
+        public Programme(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException, java.text.ParseException {
+            this.channel = getAttr(start, "channel");
+            if ("1".equals(this.channel)) this.channel = "100500";
+            this.start = DATE_FORMAT.parse(getAttr(start, "start"));
+            this.stop = DATE_FORMAT.parse(getAttr(start, "stop"));
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isStartElement()) {
+                    StartElement startElement = xmlEvent.asStartElement();
+                    String name = startElement.getName().getLocalPart();
+                    if (name.equals("title")) {
+                        title = new Title(xmlEventReader, startElement);
+                    } else if (name.equals("subtitle")) {
+                        subtitle = new Title(xmlEventReader, startElement);
+                    } else if (name.equals("desc")) {
+                        description = new Title(xmlEventReader, startElement);
+                    } else if (name.equals("category")) {
+                        categories.add(new Category(xmlEventReader, startElement));
+                    }
+                }
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public String toXml(int level) {
+            StringBuilder s = new StringBuilder(1024);
+            s.append(getIndent(level)).append("<programme");
+            s.append(" channel=\"" + escapeXml(channel) + "\"");
+            s.append(" start=\"" + DATE_FORMAT.format(start) + "\"");
+            s.append(" stop=\"" + DATE_FORMAT.format(stop) + "\"");
+            s.append(">\n");
+
+            if (title != null) {
+                s.append(title.toXml("title", level + 1));
+            }
+
+            if (subtitle != null) {
+                s.append(subtitle.toXml("sub-title", level + 1));
+            }
+
+            if (description != null) {
+                s.append(description.toXml("desc", level + 1));
+            }
+
+            for (Category c : categories) {
+                s.append(c.toXml(level + 1));
+            }
+
+            s.append(getIndent(level)).append("</programme>\n");
+            return s.toString();
+        }
+    }
+
+    public class Category {
+        //@SerializedName("@lang")
+        public String lang;
+        //@SerializedName("$")
+        public String name;
+
+        public Category(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException {
+            lang = getAttr(start, "lang");
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isCharacters()) {
+                    Characters characters = xmlEvent.asCharacters();
+                    name = characters.getData();
+                }
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public String toXml(int level) {
+            StringBuilder s = new StringBuilder(1024);
+            s.append(getIndent(level)).append("<").append("category");
+            if (lang != null) {
+                s.append(" lang=\"").append(lang).append("\"");
+            }
+            s.append(">");
+            s.append(escapeXml(name));
+            s.append("</category>\n");
+            return s.toString();
+        }
+    }
+
+    public class Title {
+        //@SerializedName("@lang")
+        public String lang;
+
+        //@SerializedName("$")
+        public String title;
+
+        public Title(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException {
+            lang = getAttr(start, "lang");
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isCharacters()) {
+                    Characters characters = xmlEvent.asCharacters();
+                    title = characters.getData();
+                }
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public String toXml(String name, int level) {
+            StringBuilder s = new StringBuilder(1024);
+            s.append(getIndent(level)).append("<").append(name);
+            if (lang != null) {
+                s.append(" lang=\"").append(lang).append("\"");
+            }
+            s.append(">");
+            s.append(escapeXml(title));
+            s.append("</").append(name).append(">\n");
+            return s.toString();
+        }
+    }
+
+    public class PreviousShow {
+        public PreviousShow() {
+            super();
+            this.start = null;
+        }
+
+        //@SerializedName("@start")
+        public String start = null;
+    }
+
+    public class EpisodeNumber {
+        //@SerializedName("@system")
+        public String system;
+
+        //@SerializedName("$")
+        public String value;
+
+    }
+
+    public class Channel {
+        //@SerializedName("@id")
+        public String id;
+        //@SerializedName("display-name")
+        public List<DisplayName> name = new ArrayList<DisplayName>();
+        //@SerializedName("icon")
+        public Icon icon;
+
+        public Channel(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException {
+            id = getAttr(start, "id");
+            if ("1".equals(id)) id = "100500";
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isStartElement()) {
+                    StartElement startElement = xmlEvent.asStartElement();
+                    String name = startElement.getName().getLocalPart();
+                    if (name.equals("display-name")) {
+                        this.name.add(new DisplayName(xmlEventReader, startElement));
+                    } else if (name.equals("icon")) {
+                        icon = new Icon(xmlEventReader, startElement);
+                    }
+                }
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        String getName() {
+            String names = "";
+            for (String name : getNameList()) {
+                names += (names.isEmpty() ? "" : ", ") + name;
+            }
+            return names;
+        }
+
+        String[] getNameList() {
+            String[] names = new String[name.size()];
+            for (int i = 0; i < name.size(); i++) {
+                names[i] = name.get(i).name;
+            }
+            return names;
+        }
+
+        public String toXml(int level) {
+            StringBuilder s = new StringBuilder(1024);
+            s.append(getIndent(level)).append("<channel");
+            s.append(" id=\"").append(id).append("\"");
+            s.append(">\n");
+
+            for (DisplayName dn : name) {
+                s.append(dn.toXml(level + 1));
+            }
+
+            if (icon != null) {
+                s.append(icon.toXml(level + 1));
+            }
+
+            s.append(getIndent(level)).append("</channel>\n");
+            return s.toString();
+        }
+
+    }
+
+    public class DisplayName {
+        //@SerializedName("@lang")
+        public String lang;
+
+        //@SerializedName("$")
+        public String name;
+
+        public DisplayName(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException {
+            lang = getAttr(start, "lang");
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isCharacters()) {
+                    Characters characters = xmlEvent.asCharacters();
+                    name = characters.getData();
+                }
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public String toXml(int level) {
+            StringBuilder s = new StringBuilder(256);
+            s.append(getIndent(level)).append("<display-name");
+            if (lang != null) {
+                s.append(" lang=\"").append(lang).append("\"");
+            }
+            s.append(">");
+            s.append(name);
+            s.append("</display-name>\n");
+            return s.toString();
+        }
+    }
+
+    public class Icon {
+        //@SerializedName("@src")
+        public String src;
+
+        //@SerializedName("@width")
+        public int width;
+
+        //@SerializedName("@height")
+        public int height;
+
+        public Icon(XMLEventReader xmlEventReader, StartElement start) throws XMLStreamException {
+            src = getAttr(start, "src");
+            width = getAttrIntValue(start, "width");
+            height = getAttrIntValue(start, "height");
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isEndElement()) {
+                    EndElement endElement = xmlEvent.asEndElement();
+                    String name = endElement.getName().getLocalPart();
+                    if (name.equals(start.getName().getLocalPart())) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public String toXml(int level) {
+            StringBuilder s = new StringBuilder(256);
+            s.append(getIndent(level)).append("<icon");
+            if (width != 0) {
+                s.append(" width=\"").append(width).append("\"");
+            }
+            if (height != 0) {
+                s.append(" height=\"").append(height).append("\"");
+            }
+            if (src != null) {
+                s.append(" src=\"").append(src).append("\"");
+            }
+            s.append("/>\n");
+            return s.toString();
+        }
+    }
+
+    public static final String DATE_FORMAT_STRING = "yyyyMMddHHmmss Z";
+    public SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_STRING);
+
+    public String getAttr(StartElement start, String attr) {
+        for (Iterator<Attribute> it = start.getAttributes(); it.hasNext(); ) {
+            Attribute a = it.next();
+            if (a.getName().getLocalPart().equals(attr)) {
+                return a.getValue();
+            }
+        }
+        return null;
+    }
+
+    public int getAttrIntValue(StartElement start, String attr) {
+        for (Iterator<Attribute> it = start.getAttributes(); it.hasNext(); ) {
+            Attribute a = it.next();
+            if (a.getName().getLocalPart().equals(attr)) {
+                try {
+                    return Integer.parseInt(a.getValue());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return 0;
+    }
+
+    static String escapeXml(String st){
+        //st = st.replaceAll("[^\\u0020-\\uD7FF\\uE000-\\uFFFD\\t\\n\\r]", " ");
+        st = st.replaceAll("&", "&amp;");
+        //st = st.replaceAll("'", "&apos;");
+        //st = st.replaceAll(">", "&gt;");
+        //st = st.replaceAll("<", "&lt;");
+        //st = st.replaceAll("\"", "&quot;");
+        return st;
+    }
+
+    static String getIndent(int level) {
+        StringBuilder s = new StringBuilder(128);
+        for (int i = 0; i < level; i++) {
+            s.append('\t');
+        }
+        return s.toString();
+    }
+
+    public XMLTV() {
+    }
+
+    public void parseStax(String filename) throws IOException {
+        BufferedInputStream fin = new BufferedInputStream(new FileInputStream(filename));
+        GZIPInputStream gzip = null;
+        if (filename.endsWith(".gz")) {
+            gzip = new GZIPInputStream(fin);
+        }
+
+        InputStream in = gzip != null ? gzip : fin;
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, (c.get(Calendar.HOUR_OF_DAY) / 6) * 6);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        long begin = c.getTime().getTime();
+        c.add(Calendar.HOUR_OF_DAY, 3 * 24);
+        long end = c.getTime().getTime();
+
+        try {
+            XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(in);
+
+            while (xmlEventReader.hasNext()) {
+                XMLEvent xmlEvent = xmlEventReader.nextEvent();
+                if (xmlEvent.isStartElement()) {
+                    StartElement start = xmlEvent.asStartElement();
+                    String name = start.getName().getLocalPart();
+                    if (name.equals("channel")) {
+                        channels.add(new Channel(xmlEventReader, start));
+                    } else if (name.equals("programme")) {
+                        try {
+                            Programme p = new Programme(xmlEventReader, start);
+                            if (p.start != null && p.stop != null
+                                    && ((p.start.getTime() >= begin && p.start.getTime() <= end) || (p.stop.getTime() >= begin && p.stop.getTime() <= end))
+                                    ) {
+                                programmes.add(p);
+                            }
+                        } catch (java.text.ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
